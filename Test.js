ab5a1bcaa8ce5c9e3277a622d45db05992f9f3cc
Sarathjithu89 <sarathjithu89@gmail.com>
2025-06-09 23:08:53 +0530

    QuickSort in place

--- Test.js
+++ Test.js
@@ -1,4 +1,4 @@
-const array = [1, 4, 5, 6, 7];
+// const array = [1, 4, 5, 6, 7];
 
 // function sum(array){
 //     if(arra.length<0)return;
@@ -10,47 +10,47 @@ const array = [1, 4, 5, 6, 7];
 //     return sum(arra.slice(-1))+
 // }
 
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-}
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
-  mid() {
-    let current = this.head;
-    if (!this.head) return;
-    let slow = this.head;
-    let fast = this.head;
-    while (fast && fast.next) {
-      slow = slow.next;
-      fast = fast.next.next;
-    }
-    console.log(slow.data);
-  }
-  add(data) {
-    const newNode = new Node(data);
-    if (!this.head) {
-      this.head = newNode;
-      return;
-    }
-    newNode.next = this.head;
-    this.head = newNode;
-  }
-}
+// class Node {
+//   constructor(data) {
+//     this.data = data;
+//     this.next = null;
+//   }
+// }
+// class LinkedList {
+//   constructor() {
+//     this.head = null;
+//   }
+//   mid() {
+//     let current = this.head;
+//     if (!this.head) return;
+//     let slow = this.head;
+//     let fast = this.head;
+//     while (fast && fast.next) {
+//       slow = slow.next;
+//       fast = fast.next.next;
+//     }
+//     console.log(slow.data);
+//   }
+//   add(data) {
+//     const newNode = new Node(data);
+//     if (!this.head) {
+//       this.head = newNode;
+//       return;
+//     }
+//     newNode.next = this.head;
+//     this.head = newNode;
+//   }
+// }
 
-const ll = new LinkedList();
+// const ll = new LinkedList();
 
-ll.add(1);
-ll.add(2);
-ll.add(5);
-ll.add(4);
-ll.add(5);
+// ll.add(1);
+// ll.add(2);
+// ll.add(5);
+// ll.add(4);
+// ll.add(5);
 
-ll.mid();
+// ll.mid();
 
 // for(let i=1;i<=10;i++){
 //     console.log(i);
@@ -85,3 +85,8 @@ ll.mid();
 // }finally{
 
 // }
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+  if (i == 5) continue;
+}
--- quickSort.js
+++ quickSort.js
@@ -1,25 +1,50 @@
-function partition(array, low, high) {
-  let pivot = array[high];
-  let i = low - 1;
-  for (let j = low; j <= high - 1; j++) {
-    if (array[j] < pivot) {
+// function partition(array, low, high) {
+//   let pivot = array[high];
+//   let i = low - 1;
+//   for (let j = low; j <= high - 1; j++) {
+//     if (array[j] < pivot) {
+//       i++;
+//       [array[i], array[j]] = [array[j], array[i]];
+//     }
+//   }
+//   [array[i + 1], array[high]] = [array[high], array[i + 1]];
+//   return i + 1;
+// }
+
+// function quickSort(array, low, high) {
+//   if (low >= high) return;
+//   let pi = partition(array, low, high);
+//   quickSort(array, low, pi - 1);
+//   quickSort(array, pi + 1, high);
+// }
+
+// let arr = [10, 80, 30, 90, 40];
+// console.log("Original array: " + arr);
+
+// quickSort(arr, 0, arr.length - 1);
+// console.log("Sorted array: " + arr);
+
+function quickSortInPlace(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    const pivotIndex = partition(arr, left, right);
+    quickSortInPlace(arr, left, pivotIndex - 1);
+    quickSortInPlace(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+function partition(arr, left, right) {
+  const pivot = arr[right];
+  let i = left - 1;
+  for (let j = left; j < right; j++) {
+    if (arr[j] < pivot) {
       i++;
-      [array[i], array[j]] = [array[j], array[i]];
+      [arr[i], arr[j]] = [arr[j], arr[i]];
     }
   }
-  [array[i + 1], array[high]] = [array[high], array[i + 1]];
+  [arr[i + 1], arr[right]] = [arr[right], arr[i + 1]];
   return i + 1;
 }
 
-function quickSort(array, low, high) {
-  if (low >= high) return;
-  let pi = partition(array, low, high);
-  quickSort(array, low, pi - 1);
-  quickSort(array, pi + 1, high);
-}
-
-let arr = [10, 80, 30, 90, 40];
-console.log("Original array: " + arr);
-
-quickSort(arr, 0, arr.length - 1);
-console.log("Sorted array: " + arr);
+const array = [0, 3, 7, 1, 6, 9, 2];
+quickSortInPlace(array);
+console.log(array);
